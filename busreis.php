695dda66b21b826f5768a278f99d89bd735c8224
stack9modz <saffgoodman@gmail.com>
2016-01-26 19:52:00 +0100

    PHP Busreis

--- busreis.php
+++ busreis.php
@@ -4,37 +4,45 @@
     <link href = "css/style.css" rel = "stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
 </head>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Soufyane
+ * Date: 1/26/2016
+ * Time: 5:23 PM
+ */
 
+$leeftijd = $_POST ["getal"];
+$prijs = 0;
+
+if ($leeftijd  >= 10 & $leeftijd <= 12) {
+    $prijs = 5;
+}
+
+if ($leeftijd = 65) {
+    $prijs = 10;
+}
+
+if ($leeftijd <= 2) {
+    $prijs = 0;
+}
+
+?>
             <body>
-
                     <section>
                         <h1>Opdracht 3: Foutmeldingen</h1>
                         <p>Een screenshot dat mijn editor (PHPstorm) foutmeldingen kan weergeven</p>
                         <img src = "img/screenshot_foutmelding.png" width="1000px"> <br>
                         <hr>
                         <h1>Opdracht 4: Busreis</h1>
-                        <?php
-                        $leeftijd =  5;
-                        $prijs = 0;
-
-                        if ($leeftijd  >= 10 & $leeftijd <= 12) {
-                            $prijs = 5;
-                        }
-                        if ($leeftijd = 65) {
-                            $prijs = 10;
-                        }
-                        if ($leeftijd <= 2) {
-                            $prijs = 0;
-                        }
-                        ?>
                         <h3>VOER JE LEEFTIJD IN</h3>
-                        <form>
-                            <input type = "text">
+                        <form action = "busreis.php" method="post">
+                            <input name ="getal"type = "text">
                             <input type="submit" value="OK">
                         </form>
                         <p>
                             <?php
-                            echo "<strong>De busreis kost ".$prijs. " euro</strong>";
+                            echo " ".$prijs. " euro";
                             ?>
                         </p>
                     </section>
--- check_busreis.php
+++ check_busreis.php
@@ -1,8 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Soufyane
- * Date: 1/26/2016
- * Time: 5:23 PM
- */
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link href = "css/style.css" rel = "stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+<section>
+    <h1>Opdracht 3: Foutmeldingen</h1>
+    <p>Een screenshot dat mijn editor (PHPstorm) foutmeldingen kan weergeven</p>
+    <img src = "img/screenshot_foutmelding.png" width="1000px"> <br>
+    <hr>
+    <h1>Opdracht 4: Busreis</h1>
+    <h3>VOER JE LEEFTIJD IN</h3>
+    <form action = "check_busreis.php" method="post">
+        <input name ="getal"type = "text">
+        <input type="submit" value="OK">
+    </form>
+    </p>
+</section>
+
+</body>
+
+
+</html>
+
 
